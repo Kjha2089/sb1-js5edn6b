686ffc6a503d8e39359e5a22c22012338dc8c56c
Kjha2089 <jhakeshav543@gmail.com>
2025-03-01 17:11:36 +0530

    src changed to dist/asset

--- src/App.tsx
+++ src/App.tsx
@@ -235,7 +235,7 @@ function App() {
                 name: 'Keshav Jha',
                 role: 'Founding Partner',
                 image:
-                  './src/Assets/Keshav.jpeg',
+                  './dist/assets/Keshav.jpeg',
                 bio: 'Keshav Jha leads commercial, civil, criminal, matrimonial practice with unparalleled expertise.',
               },
             ].map((member, index) => (
